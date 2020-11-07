91ac2ec2c0fdd7b517fd7f7602b42955c5d4c996
Adam Whitehurst <Adam.Whitehurst@live.com>
2020-11-07 15:13:22 -0800

    01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,16 +3,60 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+// function countReducer(state, newState) {
+// return newCount
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+// return state + newState
+
+// return {...state, ...newState}
+
+// return typeof newState === 'function'
+//   ? newState(state)
+//   : {...state, ...newState}
+// }
+
+function countReducer(state, action) {
+  const {type, step} = action
+  if (typeof type !== 'string')
+    throw new Error(`action.type must be a string, recieved ${typeof type}`)
+  if (typeof step !== 'number')
+    throw new Error(`action.step must be a number, recieved ${typeof step}`)
+  switch (type) {
+    case 'INCREMENT': {
+      return {...state, count: state.count + step}
+    }
+    default: {
+      throw new Error(`Unknown action.type given to countReducer: ${type}`)
+    }
+  }
+}
+
+function Counter({initialCount = 0, step = 1}) {
+  // const [count, setCount] = React.useReducer(countReducer, initialCount)
+
+  // const [count, changeCount] = React.useReducer(countReducer, initialCount)
+
+  // const [state, setState] = React.useReducer(countReducer, {
+  //   count: initialCount,
+  // })
+
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+
+  const {count} = state
+
+  const increment = () => dispatch({type: 'INCREMENT', step})
+
+  // const increment = () => setCount(count + step)
+
+  // const increment = () => changeCount(step)
+
+  // const increment = () => setState({count: count + step})
+
+  // const increment = () =>
+  //   setState(currentState => ({count: currentState.count + step}))
+
   return <button onClick={increment}>{count}</button>
 }
 
